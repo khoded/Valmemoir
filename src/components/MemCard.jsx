3cd5ddd9223aec84848ed8871fe873aba6948c5e
korede <korede@yahoo.com>
2019-06-12 21:50:01 +0100

    removed card component&redesigned card

--- src/components/MemCard.jsx
+++ src/components/MemCard.jsx
@@ -4,14 +4,12 @@ import React from 'react';
 //const Card = (props) => {
 //deconstructing props
 const MemCard = ({name, email, id}) => {
-	//const {name, email, id } = props;
 	return (
 		<div className="tc grow bg-light-white br3 pa3 ma2 dib bw2 shadow-1">
-			<img src={`https://robohash.org/${id}?200x200`} alt='robots' />
-		<div>
-		<h2>{name}</h2>
-		<p>{email}</p>
-		</div>
+			<img src={`https://robohash.org/${id}?50x50`} alt='robots' />
+			<div>
+				
+			</div>
 		</div>
 	);
 }
--- src/views/LandingPage/Sections/MemoirSection.jsx
+++ src/views/LandingPage/Sections/MemoirSection.jsx
@@ -1,14 +1,41 @@
-import React from 'react';
-import Card from "components/MemCard.jsx";
+import React from "react";
+// material-ui components
+import withStyles from "@material-ui/core/styles/withStyles";
+// core components
+import Card from "components/Card/Card.jsx";
+import CardBody from "components/Card/CardBody.jsx";
+import Button from "components/CustomButtons/Button.jsx";
+
+import imagesStyles from "assets/jss/material-kit-react/imagesStyles.jsx";
+
+import { cardTitle } from "assets/jss/material-kit-react.jsx";
+
+const style = {
+  ...imagesStyles,
+  cardTitle,
+};
+
+class Cards extends React.Component {
+  render() {
+    const { classes } = this.props;
+    return (
+      <Card style={{width: "20rem"}}>
+        <img
+          style={{height: "auto", width: "40%", display: "block"}}
+          className={classes.imgCardTop}
+          src="https://ui-avatars.com/api/?name=John+Doe"
+          alt="Card-img-cap"
+        />
+        <CardBody>
+          <h4 className={classes.cardTitle}>story title</h4>
+          <p>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <Button color="primary">Read More</Button>
+        </CardBody>
+      </Card>
 
 
-const CardList = ({ userStory }) => {
-	return (
-		<div>
-		    <Card
-				
-			/>
-	   </div>
- 	);
+    );
+  }
 }
-export default  CardList;
+
+export default withStyles(style)(Cards);
